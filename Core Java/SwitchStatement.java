20d7dc8e65bcff96dbe34265b6f23c35649eb944
Vishal <vishal.200087@gmail.com>
2026-04-16 07:58:09 +0530

    Switch Statement (With Rule Switch Example)

--- Core Java/SwitchStatement.java
+++ Core Java/SwitchStatement.java
@@ -34,15 +34,21 @@ public class SwitchStatement {
     }
 
     System.out.println("--Rule Switch-------");
-    int b = 5;
+    int b = 7;
     switch (b) {
-      case 1 -> System.out.println("Monday");
+      case 1 -> {
+        System.out.println("Monday");
+        System.out.println("Started Working Day");
+      }
       case 2 -> System.out.println("Tuesday");
       case 3 -> System.out.println("Wednesday");
       case 4 -> System.out.println("Thursday");
       case 5 -> System.out.println("Friday");
       case 6 -> System.out.println("Saturday");
-      case 7 -> System.out.println("Sunday");
+      case 7 -> {
+        System.out.println("Sunday");
+        System.out.println("Its Holiday Day");
+      }
       default -> {
         System.out.println("Invalid selection");
         // or
