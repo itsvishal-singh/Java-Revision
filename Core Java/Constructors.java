e723b75db57240f9fcfe3b42c7b4ca4e0fa49dcc
Vishal <vishal.200087@gmail.com>
2026-04-19 09:19:43 +0530

    Constructors

---
+++ Core Java/Constructors.java
@@ -0,0 +1,46 @@
+/*
+Constructor:
+-> Its look like method it-self,
+-> Same name as Class name,
+-> no return type,
+*/
+class Humann {
+  private int age;
+  private String name;
+
+  public Humann() {
+    age = 34;
+    name = "John";
+  }
+
+  public Humann(int age, String name) {
+    this.age = age;
+    this.name = name;
+  }
+
+  public int getAge() {
+    return age;
+  }
+
+  public void setAge(int age) {
+    this.age = age;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+}
+
+public class Constructors {
+  public static void main(String[] args) {
+    Humann humann = new Humann();
+    Humann humann1 = new Humann(24, "Aman");
+    System.out.println(humann.getName() + " : " + humann.getAge());
+    System.out.println(humann1.getName() + " : " + humann1.getAge());
+  }
+}
