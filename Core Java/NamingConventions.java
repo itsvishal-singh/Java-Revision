81608a8b9d22ecf28fb3c238bb8ac19e787f0f96
Vishal <vishal.200087@gmail.com>
2026-04-20 16:31:45 +0530

    Naming convention

---
+++ Core Java/NamingConventions.java
@@ -0,0 +1,22 @@
+/*
+Naming Conventions: 
+-> Camel Casing
+class and interface : Calculator, Runable
+variable and method : marks, show()
+constants - PIE, BRAND
+
+showMyMarks()
+MyData
+
+age, DATA, Human() 
+
+-> Snake Casing (supported but not prefered)
+show_my_data()
+MY_DATA
+*/
+
+public class NamingConventions {
+  public static void main(String[] args) {
+       
+  }
+}
