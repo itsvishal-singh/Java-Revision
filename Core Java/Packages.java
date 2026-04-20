6cdb26a0c16bc8b3353cbbe0d28428bcc1fdf9c0
Vishal <vishal.200087@gmail.com>
2026-04-20 22:51:54 +0530

    Packages

---
+++ Core Java/Packages.java
@@ -0,0 +1,8 @@
+ import packageExample.PackageExam;
+
+public class Packages {
+  public static void main(String[] args) {
+   PackageExam packageExam = new PackageExam();
+   packageExam.pe();
+  }
+}
---
+++ Core Java/packageExample/PackageExam.java
@@ -0,0 +1,13 @@
+/*
+Package : 
+-> A mechanism used to group related classes, interfaces, and sub-packages. 
+-> Think of it as a folder in a file directory that helps organize large projects, avoid naming conflicts, and provide access protection
+*/
+
+package packageExample;
+
+public class PackageExam {
+  public void pe(){
+    System.out.println("Example of package");
+  }
+}
