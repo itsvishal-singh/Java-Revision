f7eb31d612e9e81dbace7a9614476f1a72dca21c
Vishal <vishal.200087@gmail.com>
2026-04-22 22:26:19 +0530

    Wrapper Class

---
+++ Core Java/WrapperClass.java
@@ -0,0 +1,14 @@
+public class WrapperClass {
+  public static void main(String[] args) {
+    int num = 7;
+    Integer num1 = num; // Auto-Boxing
+
+    int num2 = num1;
+    System.out.println(num2);
+
+    String str = "12";
+    int num3 = Integer.parseInt(str);
+    System.out.println(num3*2);
+
+  }
+}
