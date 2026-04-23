d828ad08e5f2637adb58ccb50a89302f839237b6
Vishal <vishal.200087@gmail.com>
2026-04-23 17:19:53 +0530

    Types Of Interface

---
+++ Core Java/TypesOfInterface.java
@@ -0,0 +1,9 @@
+/*
+Interfaces : 
+1. Normal - two or more methods
+2. Functional (SAM - Single Abstract Method) - Only One method
+3. Marker - No method/Blank Inteface (Interface to update something to the compiler)
+*/
+public class TypesOfInterface {
+  
+}
