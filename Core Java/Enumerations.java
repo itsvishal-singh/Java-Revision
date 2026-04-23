66e84ad857a7aed71a33a0c183664a716d3c96d1
Vishal <vishal.200087@gmail.com>
2026-04-23 16:54:20 +0530

    Enumeration

---
+++ Core Java/Enumerations.java
@@ -0,0 +1,32 @@
+enum Laptop {
+  Macbook(2000), XPS(2200), Surface, ThinkPad(1800);
+
+  private int price;
+
+  private Laptop(){
+    price = 500;
+  }
+
+  private Laptop(int price) {
+    this.price = price;
+  }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+
+}
+
+public class Enumerations {
+  public static void main(String[] args) {
+    // Laptop lap = Laptop.Macbook;
+    // System.out.println(lap + " : " + lap.getPrice());
+    for(Laptop lap : Laptop.values())
+      System.out.println(lap + " : " + lap.getPrice());
+  }
+}
