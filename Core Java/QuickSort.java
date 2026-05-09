1f040603da87f508d929b91d26ddec3a2e2d4f94
Vishal <vishal.200087@gmail.com>
2026-05-09 09:13:21 +0530

    Quick Sort

---
+++ Core Java/QuickSort.java
@@ -0,0 +1,36 @@
+public class QuickSort {
+
+  public static void quickSort(int[] array, int low, int high) {
+    if (low < high) {
+      int partiTion = partition(array, low, high);
+      quickSort(array, low, partiTion - 1);
+      quickSort(array, partiTion + 1, high);
+    }
+  }
+
+  private static int partition(int[] array, int low, int high) {
+    int pivot = array[high];
+    int i = low - 1;
+    for (int j = low; j < high; j++) {
+      if (array[j] < pivot) {
+        i++;
+        int temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+      }
+    }
+    int temp = array[i + 1];
+    array[i + 1] = array[high];
+    array[high] = temp;
+
+    return i + 1;
+  }
+
+  public static void main(String[] args) {
+    int[] array = { 51, 13, 62, 75, 32, 49 };
+    quickSort(array, 0, array.length - 1);
+    for (int arr : array)
+      System.out.print(arr + " ");
+  }
+
+}
