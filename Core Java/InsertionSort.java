f66516305c2151e9c0c17d80690e3dcc85b822ca
Vishal <vishal.200087@gmail.com>
2026-05-09 07:50:24 +0530

    Insertion Sort

--- Core Java/InsertionSort.java
+++ Core Java/InsertionSort.java
@@ -1,3 +1,28 @@
 public class InsertionSort {
-  
+  public static void main(String[] args) {
+    int[] nums = { 6, 3, 8, 4, 1 };
+
+    System.out.println("---Before Sorting-----");
+    for (int num : nums)
+      System.out.print(num + ", ");
+
+    System.out.print("\n---Iterations---");
+    for (int i = 0; i < nums.length; i++) {
+      int key = nums[i];
+      int j = i - 1;
+      while (j >= 0 && nums[j] > key) {
+        nums[j + 1] = nums[j];
+        j--;
+      }
+      nums[j + 1] = key;
+
+      System.out.println();
+      for (int num : nums)
+        System.out.print(num + ", ");
+    }
+
+    System.out.println("\n---After Sorting-----");
+    for (int num : nums)
+      System.out.print(num + ", ");
+  }
 }
