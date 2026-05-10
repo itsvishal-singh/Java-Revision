39347f837d5567d2af7c5a4b2957e06d3c3ccf9e
Vishal <vishal.200087@gmail.com>
2026-05-10 14:12:28 +0530

    Tree

---
+++ Core Java/Tree.java
@@ -0,0 +1,57 @@
+class Node {
+  int data;
+  Node left;
+  Node right;
+
+  public Node(int data) {
+    this.data = data;
+  }
+}
+
+class BinaryTree {
+  Node root;
+
+  void insert(int data) {
+    root = insertRec(root, data);
+  }
+
+  public Node insertRec(Node root, int data) {
+    if (root == null)
+      root = new Node(data);
+    else if (data < root.data) {
+      root.left = insertRec(root.left, data);
+    } else if (data > root.data) {
+      root.right = insertRec(root.right, data);
+    }
+    return root;
+  }
+
+  public void inorder() {
+    inorderRec(root);
+  }
+
+  public void inorderRec(Node root) {
+    if (root != null) {
+
+      inorderRec(root.left);
+      System.out.print(root.data + " ");
+      inorderRec(root.right);
+    }
+  }
+
+}
+
+public class Tree {
+  public static void main(String[] args) {
+    BinaryTree tree = new BinaryTree();
+    tree.insert(8);
+    tree.insert(7);
+    tree.insert(12);
+    tree.insert(15);
+    tree.insert(2);
+    tree.insert(5);
+
+    tree.inorder();
+
+  }
+}
